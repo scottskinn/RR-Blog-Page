8042c3f397eebc3eea3f1d5964e20e02c365feed
scottskinn <84105761+scottskinn@users.noreply.github.com>
2023-06-12 16:34:20 -0400

    create new actrcle page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-  
-  get "articles", to: "articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  resources :articles
+  
 end
